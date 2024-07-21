bcec8355ceb742fec55a3c9695fcde7679ef1895
Agathe <agathe.leroux.ece@gmail.com>
2024-07-21 21:56:58 +0200

    create nx workspace and add host with ssr application with nx console

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-empty
+
+
+# https://www.youtube.com/watch?v=edTA7amdvzI
+
+ npx create-nx-workspace@^15 angular-monorepo --preset=angular-monorepo
+
+ nx console : g host
+ 
+
---
+++ apps/host/src/app/nx-welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'angular-monorepo-nx-welcome',
+  template: `
+    host entry component
+  `,
+  styles: [],
+  encapsulation: ViewEncapsulation.None,
+})
+export class NxWelcomeComponent {}
